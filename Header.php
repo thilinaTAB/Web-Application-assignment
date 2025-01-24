ad14a0343141af203a45b812d76dde2666515f32
Thilina Bowatta <bowattatab@gmail.com>
2025-01-25 00:00:33 +0530

    first commit

---
+++ Header.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="eng">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta
+      name="description"
+      content="Orbitor,business,company,agency,modern,bootstrap4,tech,software"
+    />
+    <meta name="author" content="themefisher.com" />
+
+    <title>Care Compass Hospitals</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
+
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="plugins/bootstrap/css/font/bootstrap-icons.css" />
+    <!-- Icon Font Css -->
+    <link rel="stylesheet" href="plugins/icofont/icofont.min.css" />
+    <!-- Slick Slider  CSS -->
+    <link rel="stylesheet" href="plugins/slick-carousel/slick/slick.css" />
+    <link
+      rel="stylesheet"
+      href="plugins/slick-carousel/slick/slick-theme.css"
+    />
+
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="css/style.css" />
+
+    
+  </head>
+
+  <body id="top">
+    <header>
+      <div class="header-top-bar">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <ul class="top-bar-info list-inline-item pl-0 mb-0">
+                <li class="list-inline-item">
+                  <a href="mailto:support@gmail.com"
+                    ><i class="icofont-support-faq mr-2"></i
+                    >support@cchospitals.com</a
+                  >
+                </li>
+                <li class="list-inline-item">
+                  <i class="icofont-location-pin mr-2"></i
+                  >Kandy | Colombo | Kurunegala
+                </li>
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <div class="text-lg-right top-right-bar mt-2 mt-lg-0">
+           
+                <a href="tel:5959">
+                <i class="bi bi-telephone-plus"></i>
+                  <span class="h4">Imergency Call : </span>
+                  <span class="h3">5959</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav class="navbar navbar-expand-lg navigation" id="navbar">
+        <div class="container">
+          <a class="navbar-brand" href="index.php">
+            <img src="images/CCH_LOGO.png" alt="logo" class="img-fluid" />
+          </a>
+
+          <button
+            class="navbar-toggler collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarmain"
+            aria-controls="navbarmain"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="icofont-navigation-menu"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarmain">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="About.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="service.html">Services</a>
+              </li>
+
+              
+
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="doctor.html"
+                  id="dropdown03"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  >Doctors <i class="icofont-thin-down"></i
+                ></a>
+                <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                  <li>
+                    <a class="dropdown-item" href="doctor.html">Doctors</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="doctor-single.html"
+                      >Doctor Single</a
+                    >
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="appoinment.html"
+                      >Appoinment</a
+                    >
+                  </li>
+                </ul>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="blog-sidebar.html"
+                  id="dropdown05"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  >Feedbacks <i class="icofont-thin-down"></i
+                ></a>
+                <ul class="dropdown-menu" aria-labelledby="dropdown05">
+                  <li>
+                    <a class="dropdown-item" href="blog-sidebar.html"
+                      >Blog with Sidebar</a
+                    >
+                  </li>
+
+                  <li>
+                    <a class="dropdown-item" href="blog-single.html"
+                      >Blog Single</a
+                    >
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+include_once 'Header.php';
+?>
+<!-- Slider Start -->
+ <section class="banner">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 col-md-12 col-xl-7">
+        <div class="block">
+          <div class="divider mb-3"></div>
+          <span class="text-uppercase text-sm letter-spacing">
+            Your Complete Health Care Partner
+          </span>
+
+          <h1 class="mb-3 mt-3">
+            Empowering Your Journey to Better Health
+          </h1>
+<!---->
+          <p class="mb-4 pr-5">
+            A repudiandae ipsam labore ipsa voluptatum quidem quae laudantium 
+            quisquam aperiam maiores sunt fugit, deserunt rem suscipit placeat.
+              </p>
+                       
+<div class="btn-container">
+                <a
+                  href="Appointment.php"
+                  target="_blank"
+                  class="btn btn-main-2 btn-icon btn-round-full"
+                  >Make an Appoinment <i class="icofont-simple-right ml-2"></i
+                ></a>
+              </div>   
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <?php
+include_once 'Footer.php';
+?>
